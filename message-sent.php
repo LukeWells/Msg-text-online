c5a34ba006c969d8782b093c020ff1646cbd8295
Luke <39895263+LukeWells@users.noreply.github.com>
2018-08-06 15:11:52 +1000

    minor update

--- message-sent.php
+++ message-sent.php
@@ -10,25 +10,13 @@
 		ini_set('display_startup_errors', 1);
 		error_reporting(E_ALL);
 
-		$client = new http\Client;
-		$request = new http\Client\Request;
+		$request = new HttpRequest();
+		$request->setUrl('https://api.whispir.com/messages');
+		$request->setMethod(HTTP_METH_POST);
 
-		$body = new http\Message\Body;
-		$to = $_POST['phoneNumber'];
-		$subject = $_POST['subject'];
-		$message = $_POST['messageText'];
-
-		$body->append('{"to":"","subject":"","body":""}');
-
-		$body->to = $to;
-		$body->subject = $subject;
-		$body->body = $messageText;
-
-		$request->setRequestUrl('https://api.whispir.com/messages');
-		$request->setRequestMethod('POST');
-		$request->setBody($body);
-
-		$request->setQuery(new http\QueryString(array('apikey' => 'vaduqqatayakcjh6t9v5mnhf')));
+		$request->setQueryData(array(
+		  'apikey' => 'vaduqqatayakcjh6t9v5mnhf'
+		));
 
 		$request->setHeaders(array(
 		  'content-type' => 'application/vnd.whispir.message-v1+json',
@@ -36,10 +24,23 @@
 		  'authorization' => 'Basic bHVrZS53ZWxsczpoM3JkSDB1c2U='
 		));
 
-		$client->enqueue($request)->send();
-		$response = $client->getResponse();
+		$to = $_POST['phoneNumber'];
+		$subject = $_POST['subject'];
+		$message = $_POST['messageText'];
 
-		echo $response->getBody();
+		$request->setBody('{"to":"","subject":"","body":""}');
+
+		$request->to = $to;
+		$request->subject = $subject;
+		$request->body = $messageText;
+
+		try {
+		  $response = $request->send();
+
+		  echo $response->getBody();
+		} catch (HttpException $ex) {
+		  echo $ex;
+		}
 	?>
 	<div class="container">
 		<h3>Mobile phone messaging service</h3>
