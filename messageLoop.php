c75423717ffe2bb338c349b80c94bc98214e52ab
Luke <39895263+LukeWells@users.noreply.github.com>
2018-08-15 09:14:46 +1000

    update

--- messageLoop.php
+++ messageLoop.php
@@ -4,7 +4,8 @@
 	$whispirMessage = array('messageIDplaceholder');
 
 	foreach ($whispirMessage as list($messageId)) {
-
+		
+		$curl = curl_init();
 		curl_setopt($curl,CURLOPT_URL,"https://api.whispir.com/messages/".$messageId."/messageresponses?view=detailed&filter=default&apikey=".$api);
 
 		curl_setopt_array($curl, array(
@@ -14,9 +15,9 @@
 				CURLOPT_TIMEOUT => 30,
 				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 				CURLOPT_HTTPHEADER => array(
-					"accept: application/vnd.whispir.message-v1+json",
+					"accept: application/vnd.whispir.messageresponse-v1+json",
 					"authorization: Basic bHVrZS53ZWxsczpoM3JkSDB1c2U=",
-					"content-type: application/vnd.whispir.message-v1+json"
+					"content-type: application/vnd.whispir.messageresponse-v1+json"
 				),
 		));
 
--- responses.php
+++ responses.php
@@ -16,14 +16,45 @@
 		error_reporting(E_ALL);
 		
 		include ('api.php'); 
+		/*
 		include("messageLoop.php");
+		*/
 
-		if ($err) {
-			echo "cURL Error #:" . $err;
-		} else {
-			echo $response;
-		}	
-	?>
+		$response = get_web_page("https://api.whispir.com/messages?apikey=".$api);
+		$resArr = array();
+		$resArr = json_decode($response);
+		echo "<pre>"; print_r($resArr); echo "</pre>";
+
+		function get_web_page($url) {
+			$options = array(
+				CURLOPT_RETURNTRANSFER => true,   // return web page
+				CURLOPT_HTTPHEADER => array(
+					"accept: application/vnd.whispir.message-v1+json",
+					"authorization: Basic bHVrZS53ZWxsczpoM3JkSDB1c2U=",
+					"content-type: application/vnd.whispir.message-v1+json"
+				), 
+				CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+				CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+				CURLOPT_ENCODING       => "",     // handle compressed
+				CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+				CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+				CURLOPT_TIMEOUT        => 120,    // time-out on response
+			); 
+
+			$ch = curl_init($url);
+			curl_setopt_array($ch, $options);
+
+			$content  = curl_exec($ch);
+			$errors = curl_error($ch);
+			$response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+			curl_close($ch);
+
+			var_dump($errors);
+
+			return $content;
+		}
+		?>
 		<a href="index.php" class="btn btn-secondary">Back</a>
 	</div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
